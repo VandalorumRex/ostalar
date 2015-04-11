6dbf920508760aaa44d2b443a992d12c36442436
Mansur <Mansur>
2015-04-11 23:07:19 +0300

    debian

---
+++ urnak/toep.php
@@ -0,0 +1,9 @@
+<div class="divar">
+    <div class="aq">
+        <h1><?=$APPLICATION->title?></h1>
+            <pre>
+                <?=($_SERVER['HTTP_HOST']=='ostalar.tk')?'':print_r($_SERVER);?>    
+            </pre>
+        
+    </div>
+</div>
