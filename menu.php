cef1a1999295bd0ca034ef0d85bce2044b4264e6
Mansur <Mansur>
2015-04-11 22:27:03 +0300

    debian

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <?
 $menu = simplexml_load_string(file_get_contents("menu.xml"));
-//print '<pre>';//print_r($xml);print '</pre>';
+//print '<pre>';print_r($menu);print '</pre>';
 ?>
 <div id="bash_menu">
 	<div class="uzak">
@@ -12,9 +12,13 @@ $menu = simplexml_load_string(file_get_contents("menu.xml"));
                 <li><a href="<?=$href?>"><?=$it['@attributes']['name']?></a>
                 <?if(isset($it['subitem'])):?>
                     <ul>
-                    <?foreach($it['subitem'] as $subitem):?>
-                        <li><a href="/<?=$it['@attributes']['name']?>/<?=$subitem?>"><?=$subitem?></a></li>
-                    <?endforeach?>
+                    <?if(is_array($it['subitem'])):?>
+                        <?foreach($it['subitem'] as $subitem):?>
+                            <li><a href="/<?=$it['@attributes']['name']?>/<?=$subitem?>"><?=$subitem?></a></li>
+                        <?endforeach?>
+                    <?else:?>
+                        <li><a href="/<?=$it['@attributes']['name']?>/<?=$it['subitem']?>"><?=$it['subitem']?></a></li>
+                    <?endif?>
                     </ul>
                 <?endif?>
                 </li>
