452148f8ce977826be02e63da2d04e9344cd0fbf
Mansur <Mansur>
2015-04-11 22:17:11 +0300

    debian

--- basqych/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "../bash.php";
-?>
-<div class="divar">
-    <div class="aq">
-        <h1>Изготавливаем лестницы</h1>    
-    </div>
-</div>
--- ishek/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "../bash.php";
-?>
-<div class="divar">
-    <div class="aq">
-        <h1>Изготавливаем столы</h1>    
-    </div>
-</div>
