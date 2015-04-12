ad3f0127d26594df9e6b96f1eb77c0040e005e83
Mansur <Mansur>
2015-04-12 20:42:20 +0300

    debian

---
+++ ajaq.php
@@ -0,0 +1,9 @@
+<div id="ajaq">
+    <div align="center" style="margin-top:20px;">
+       <a style="color:white" href="http://www.dot.tk" target="_blank">Получить домен *.tk</a> 
+    </div>
+    
+<?php
+
+?>
+</div>
