2e70e79024a50775913047bfe0d41551bc319b68
Mansur <gho-mansur@yandex.ru>
2015-04-14 17:05:46 +0300

    Mansur work

--- urnak/taswir.php
+++ urnak/taswir.php
@@ -3,7 +3,7 @@ $data = simplexml_load_string(file_get_contents("urnak/taswir.xml"));
 print '<pre>';//print_r($data);
 foreach($data as $item){
 	//print_r($item);
-	print($item['name']);
+	print($item->name);
 }
 ?>
 <div class="divar">
