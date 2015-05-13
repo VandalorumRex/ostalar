950aeee44bba8223c3c21f05fcfb9a1ff2d18f75
Mansur <gho-mansur@yandex.ru>
2015-05-13 21:06:15 +0300

    before

---
+++ urnak/taswir.php
@@ -0,0 +1,16 @@
+<?
+$data = simplexml_load_string(file_get_contents("urnak/taswir.xml"));
+//print '<pre>';//print_r($data);
+foreach($data as $item){
+	//print_r($item);
+	//print($item->name);
+	if($item->name==$url[2]) {
+		$taswir = $item->subitem->taswir;
+	}
+}
+?>
+<div class="divar">
+    <div class="aq">
+		<?=$taswir?>
+    </div>
+</div>
