337facf4877762eb5b3ebca8015ef9da22116245
Mansur <gho-mansur@yandex.ru>
2015-05-15 21:14:54 +0300

    bash bit

--- urnak/toep.php
+++ urnak/toep.php
@@ -9,5 +9,31 @@
         Без осталар. Йортлар төзибез, җиһаз, баскычлар, ишекләр ясыйбыз. Барлык эшләребезгә гарантия бирәбез.
         <?endif?>
         </div>
+        <?echo iconv('utf8','cp1251','<script>var surat = ["'.implode("\",\"",$suratlar).'"]</script>');?>
+        <?/*foreach($suratlar as $surat):?>           
+            <img src="<?=$surat?>" width="200px" height="100px"/>
+        <?endforeach*/?>
+        <div id="surat">
+           <!--img id="rasem" src="<?=$suratlar[0]?>" width="960px" height="480px"/--> 
+		   <div id="rasem" style="border-radius:30px;clear:both;margin-left:auto;margin-right:auto;width:960px;height:720px;background:url(<?=$suratlar[0]?>) no-repeat;background-size:contain;"></div>
+        </div>
+        <br/>
+        <script>           
+        setInterval(function(){
+            //$('#rasem').hide("slow");
+            //$('#rasem').attr('src', surat[i]);
+			//i++;
+			if (surat.length<=i) {
+				i=0;
+			}
+			$('#rasem').css('background','url('+surat[i]+') no-repeat');
+			$('#rasem').css('background-size','contain');
+			i++;
+            //$('#rasem').show("slow");
+            
+            //alert(surat[i]);
+        },
+                    7000);
+        </script>
     </div>
 </div>
