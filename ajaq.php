cd73860bd9f625a843a630d4f894fe81428a6685
Mansur <gho-mansur@yandex.ru>
2015-05-15 15:33:25 +0300

    Mansur work

--- ajaq.php
+++ ajaq.php
@@ -1,7 +1,7 @@
 <div id="ajaq">
     <div align="center" style="margin-top:20px;">
        <!--a style="color:white" href="http://www.dot.tk" target="_blank">Получить домен *.tk</a-->
-       (843) 245-48-03 - Ильнар
+       (843) 245-48-03 - Ильнар, +7-960-038-56-73 - Рафаэль
     </div>
     
 <?php
