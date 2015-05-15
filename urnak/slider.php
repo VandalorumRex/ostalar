030ddd1cd790006bcc0d7dfca2a96cb70fab6780
Mansur <gho-mansur@yandex.ru>
2015-05-15 21:23:29 +0300

    butan format

--- urnak/slider.php
+++ urnak/slider.php
@@ -3,11 +3,14 @@
         <script> var i=0;</script>
         <h1 id="title"><?=$APPLICATION->title?></h1>
         <?echo iconv('utf8','cp1251','<script>var surat = ["'.implode("\",\"",$suratlar).'"]</script>');?>
-        <?/*foreach($suratlar as $surat):?>           
+		<div class="clear"></div>
+		<div align="center">
+        <?foreach($suratlar as $surat):?>           
             <img src="<?=$surat?>" width="200px" height="100px"/>
-        <?endforeach*/?>
-        <div id="surat">
-           <!--img id="rasem" src="<?=$suratlar[0]?>" width="960px" height="480px"/--> 
+        <?endforeach?>
+		</div>
+        <?/*div id="surat">
+          
 		   <div id="rasem" style="border-radius:30px;clear:both;margin-left:auto;margin-right:auto;width:960px;height:720px;background:url(<?=$suratlar[0]?>) no-repeat;background-size:contain;"></div>
         </div>
         <br/>
@@ -27,6 +30,6 @@
             //alert(surat[i]);
         },
                     7000);
-        </script>
+        </script*/?>
     </div>
 </div>
