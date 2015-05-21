404f501d15b100e0636719a9f21760ef55173259
Mansur <gho-mansur@yandex.ru>
2015-05-21 15:40:43 +0300

    transform

--- urnak/slider.php
+++ urnak/slider.php
@@ -11,27 +11,5 @@
 			</a>
         <?endforeach?>
 		</div>
-        <?/*div id="surat">
-          
-		   <div id="rasem" style="border-radius:30px;clear:both;margin-left:auto;margin-right:auto;width:960px;height:720px;background:url(<?=$suratlar[0]?>) no-repeat;background-size:contain;"></div>
-        </div>
-        <br/>
-        <script>           
-        setInterval(function(){
-            //$('#rasem').hide("slow");
-            //$('#rasem').attr('src', surat[i]);
-			//i++;
-			if (surat.length<=i) {
-				i=0;
-			}
-			$('#rasem').css('background','url('+surat[i]+') no-repeat');
-			$('#rasem').css('background-size','contain');
-			i++;
-            //$('#rasem').show("slow");
-            
-            //alert(surat[i]);
-        },
-                    7000);
-        </script*/?>
     </div>
 </div>
