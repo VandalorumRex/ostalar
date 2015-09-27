6591ad7657b3550ae71a95546e475efe69109c30
Mansur <gho-mansur@yandex.ru>
2015-09-27 23:22:37 +0300

    master debian home

--- urnak/zakaz.php
+++ urnak/zakaz.php
@@ -9,8 +9,9 @@
     <input name="zakaz_jasaw" value="<?=$zakaz[$tel]?>" type="submit" />
 </form>
 <script>
-    $j = jQuery.noConflict();
+    $('#tilifun').mask("+7(999) 999-99-99");
+    /*$j = jQuery.noConflict();
 $j(function(){
     $j('#tilifun').mask("+7(999) 999-99-99");
-});
+});*/
 </script>
