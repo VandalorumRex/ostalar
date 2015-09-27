eec04b3df6ba18a8e5da33207fc1fcdd71790ee2
Mansur <gho-mansur@yandex.ru>
2015-09-27 23:54:01 +0300

    master debian home

--- index.php
+++ index.php
@@ -57,22 +57,22 @@ if($xat=='kitte'){
 }
 ?>
 <script>
-$('#tilifun').mask("+7(999) 999-99-99");
 $(document).ready(function() {
-        $("a.gallery").fancybox({						
-                "padding" : 20,
-                "imageScale" : false, 
-                "zoomOpacity" : false,
-                "zoomSpeedIn" : 1000,	
-                "zoomSpeedOut" : 1000,	
-                "zoomSpeedChange" : 1000, 
-                "frameWidth" : 700,	 
-                "frameHeight" : 600, 
-                "overlayShow" : true, 
-                "overlayOpacity" : 0.8,	
-                "hideOnContentClick" :false,
-                "centerOnScroll" : false
+    //$('#tilifun').mask("+7(999) 999-99-99");
+    $("a.gallery").fancybox({						
+            "padding" : 20,
+            "imageScale" : false, 
+            "zoomOpacity" : false,
+            "zoomSpeedIn" : 1000,	
+            "zoomSpeedOut" : 1000,	
+            "zoomSpeedChange" : 1000, 
+            "frameWidth" : 700,	 
+            "frameHeight" : 600, 
+            "overlayShow" : true, 
+            "overlayOpacity" : 0.8,	
+            "hideOnContentClick" :false,
+            "centerOnScroll" : false
 
-                });
+            });
 });
 </script>
