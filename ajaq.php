014b16dcde8a7103fed3046166dcd5a4f9ac6ac2
Mansur <gho-mansur@yandex.ru>
2015-09-28 00:19:27 +0300

    master debian home

--- ajaq.php
+++ ajaq.php
@@ -2,6 +2,40 @@
     <div align="center" style="margin-top:20px;">
        <!--a style="color:white" href="http://www.dot.tk" target="_blank">Получить домен *.tk</a-->
        (843) 245-48-03, (843) 245-35-51
+       
+    </div>
+    <div align="center">
+        <!-- Yandex.Metrika informer -->
+        <a href="https://metrika.yandex.ru/stat/?id=29640165&amp;from=informer"
+        target="_blank" rel="nofollow"><img src="//informer.yandex.ru/informer/29640165/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+        style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:29640165,lang:'ru'});return false}catch(e){}"/></a>
+        <!-- /Yandex.Metrika informer -->
+
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter29640165 = new Ya.Metrika({id:29640165,
+                            clickmap:true,
+                            trackHash:true});
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks");
+        </script>
+        <noscript><div><img src="//mc.yandex.ru/watch/29640165" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
     </div>
     
 <?php
