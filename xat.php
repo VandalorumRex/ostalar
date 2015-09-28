0a046b49f99bb321478af18e547f882ffb89d4ff
Mansur Usmanov <gho-mansur@yandex.ru>
2015-09-28 07:24:22 +0300

    Mansur work master

--- xat.php
+++ xat.php
@@ -2,11 +2,16 @@
 $bit = 'http://'.filter_input(INPUT_SERVER,"HTTP_HOST").filter_input(INPUT_SERVER,"REQUEST_URI");
 $to = 'gho-mansur@yandex.ru, bashirovilnar@mail.ru';
 $subject = 'Заказ';
-//if($tel=='ru')
+if($tel=='ru')
 {
     $message = 'Имя: '.filter_input(INPUT_POST, 'isem')."\n"
             .'Телефон: '.filter_input(INPUT_POST, 'tilifun')."\n"
-            .'Страница заявки: '.$bit;
+            .'Интерес: '.filter_input(INPUT_SERVER,"REQUEST_URI");
+}
+else{
+    $message = 'Исеме: '.filter_input(INPUT_POST, 'isem')."\n"
+            .'Телефоны: '.filter_input(INPUT_POST, 'tilifun')."\n"
+            .'Кызыксына: '.filter_input(INPUT_SERVER,"REQUEST_URI");
 }
 $xat = mail($to, $subject, $message);
 if($xat){
