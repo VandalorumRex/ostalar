a9792791e4e0c5e28f1c036cf31420e13e323fb0
Mansur Usmanov <gho-mansur@yandex.ru>
2016-01-26 21:46:11 +0300

    tel

--- urnak/zakaz.php
+++ urnak/zakaz.php
@@ -5,6 +5,6 @@
 ?>
 <form method="post">
     <input name="isem" placeholder="<?=$isem[$tel]?>" type="text" required="" />
-    <input name="tilifun" id="tilifun" placeholder="<?=$tilifun[$tel]?>" type="text" required="" />
+    <input name="tilifun" id="tilifun" placeholder="<?=$tilifun[$tel]?>" type="tel" required="" />
     <input name="zakaz_jasaw" value="<?=$zakaz[$tel]?>" type="submit" />
 </form>
