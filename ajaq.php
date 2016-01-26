87a7f29ff75085e24b540b68bad6d13c69734721
Mansur Usmanov <gho-mansur@yandex.ru>
2016-01-26 20:37:28 +0300

    tel

--- ajaq.php
+++ ajaq.php
@@ -3,7 +3,7 @@
        <!--a style="color:white" href="http://www.dot.tk" target="_blank">Получить домен *.tk</a-->
        <!--(843) 245-48-03, (843) 245-35-51-->
 	    <?php foreach($phone as $tel):?>
-			<a href="tel:<?php=str_replace(array(' ','(',')','-',$tel),'',?>"><?=$tel?></a>&nbsp;&nbsp;&nbsp;
+			<a href="tel:<?=str_replace(array(' ','(',')','-',$tel),'',?>"><?=$tel?></a>&nbsp;&nbsp;&nbsp;
 		<?php endforeach;?>
     </div>
     <div align="center">
