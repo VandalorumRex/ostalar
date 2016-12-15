8015f1c151632ef2076e84194fcb49a55da84a54
Mansur Usmanov <gho-mansur@yandex.ru>
2016-12-15 09:37:59 +0300

    taswir

--- urnak/slider.php
+++ urnak/slider.php
@@ -2,17 +2,35 @@
     <div class="aq">
         <script> var i=0;</script>
         <h1 id="title"><?=$APPLICATION->title?></h1>
+        <?php $isemnar = null;
+        if(isset($taswirlar[$url[2]])):
+            $isemnar = array_keys($taswirlar[$url[2]]); 
+         endif?>
         <?php include("urnak/zakaz.php");
-        //echo '<script>var surat = ["'.implode("\",\"",$suratlar).'"]</script>';
+        
         echo iconv('utf8','cp1251','<script>var surat = ["'.implode("\",\"",$suratlar).'"]</script>');?>
 		<div class="clear"></div>
-		
-        <?php foreach($suratlar as $key=>$surat):?>
+	<?php if($isemnar):
+            $j = 0; $max = 3;
+            foreach ($isemnar as $key=>$isem):?>
+                <div class="suz"><?php echo $taswirlar[$url[2]][$isem]?></div>
+                <div align="center">
+                <?php for($i=0;$i<$max;$i++):?>
+                    <a class="gallery" rel="group" title="<?=$APPLICATION->title?>" href="<?=$suratlar[$i+$j]?>">
+			<img src="<?=$kechkena[$i+$j]?>" height="300px"/>
+                    </a>
+                <?php endfor;
+                $j=$j+$max;?>
+                </div>
+        <?php endforeach;
+            else:
+            foreach($suratlar as $key=>$surat):?>
                             
 			<a class="gallery" rel="group" title="<?=$APPLICATION->title?>" href="<?=$surat?>">
 				<img src="<?=$kechkena[$key]?>" height="200px"/>
 			</a>
                             
-        <?php endforeach?>		
+        <?php endforeach;
+        endif;?>		
     </div>
 </div>
--- urnak/zakaz.php
+++ urnak/zakaz.php
@@ -3,8 +3,8 @@
     $tilifun = array("ru" => "ВАШ ТЕЛЕФОН", "tt" => "ТЕЛЕФОНЫГЫЗ");
     $zakaz = array("ru" => "СДЕЛАТЬ ЗАКАЗ", "tt" => "ЗАКАЗ ЯСЫЙМ");
 ?>
-<form method="post">
-    <input name="isem" placeholder="<?=$isem[$tel]?>" type="text" required="" />
-    <input name="tilifun" id="tilifun" placeholder="<?=$tilifun[$tel]?>" type="tel" required="" />
-    <input name="zakaz_jasaw" value="<?=$zakaz[$tel]?>" type="submit" />
+<form method="post" align="center">
+    <p><input name="isem" placeholder="<?=$isem[$tel]?>" type="text" required="" /></p>
+    <p><input name="tilifun" id="tilifun" placeholder="<?=$tilifun[$tel]?>" type="tel" required="" /></p>
+    <p><input name="zakaz_jasaw" value="<?=$zakaz[$tel]?>" type="submit" /></p>
 </form>
