fe139d51a82a7dfe58068d961d181f4aa3bfdf37
Mansur Usmanov <gho-mansur@yandex.ru>
2016-12-19 13:11:08 +0300

    google search

--- bash.php
+++ bash.php
@@ -64,23 +64,13 @@
    </div>
 </div>
 <?php if(strpos(filter_input(INPUT_SERVER,"HTTP_HOST"),".local")===false):?>
-<!--script>
+<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-63078283-1', 'auto');
-  ga('send', 'pageview');
-
-</script-->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-63078283-2', 'auto');
   ga('send', 'pageview');
 
 </script>
