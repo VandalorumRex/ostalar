760adaacb28cadab8dcdf1af0ae966cd1f29060a
Mansur Usmanov <gho-mansur@yandex.ru>
2016-12-19 12:44:29 +0300

    google search

--- bash.php
+++ bash.php
@@ -63,8 +63,8 @@
 		<div class="clear"></div>
    </div>
 </div>
-<?//if($body_domain=='ostalar'):?>
-<script>
+<?php if(strpos(filter_input(INPUT_SERVER,"HTTP_HOST"),".local")===false):?>
+<!--script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -73,5 +73,15 @@
   ga('create', 'UA-63078283-1', 'auto');
   ga('send', 'pageview');
 
+</script-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-63078283-2', 'auto');
+  ga('send', 'pageview');
+
 </script>
-<?//endif?>
+<?php endif?>
