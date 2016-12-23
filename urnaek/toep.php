745f344fc9bcdc1acf62fb53d898425630706288
Mansur Usmanov <gho-mansur@yandex.ru>
2016-12-23 21:39:34 +0300

    width="335px" height="335px"

--- urnaek/toep.php
+++ urnaek/toep.php
@@ -13,9 +13,9 @@
             <a href="<?=Törlər::$JULLAR[$APPLICATION->tele][Törlər::YORTLAR]?>"><?=$APPLICATION->barysynQaraw()?></a>
         </div>
         <div class="rows">
-            <img class="line__image" src="/rasem/yort/0769.jpg" />
-            <img class="line__image" src="/rasem/yort/0785.jpg" />
-            <img class="line__image" src="/rasem/yort/0793.jpg" />
+            <img class="line__image" src="/rasem/yort/0769.jpg" width="335px" height="335px"/>
+            <img class="line__image" src="/rasem/yort/0785.jpg" width="335px" height="335px"/>
+            <img class="line__image" src="/rasem/yort/0793.jpg" width="335px" height="335px"/>
         </div>
     </div>
     <div class="rows line">
@@ -23,9 +23,9 @@
             <a href="<?=Törlər::$JULLAR[$APPLICATION->tele][Törlər::BASQYCHLAR]?>"><?=$APPLICATION->barysynQaraw()?></a>
         </div>
         <div class="rows">
-            <img class="line__image" src="/rasem/basqych/20140927_164447.png" />
-            <img class="line__image" src="/rasem/basqych/88.png" />
-            <img class="line__image" src="/rasem/basqych/79.png" />
+            <img class="line__image" src="/rasem/basqych/20140927_164447.png" width="335px" height="335px"/>
+            <img class="line__image" src="/rasem/basqych/88.png" width="335px" height="335px"/>
+            <img class="line__image" src="/rasem/basqych/79.png" width="335px" height="335px"/>
         </div>
     </div> 
 </div>
