83a703f604bf09dac11599a6e85a3a3fffa22f31
Mansur Usmanov <gho-mansur@yandex.ru>
2016-12-23 22:12:50 +0300

    zakaz

--- urnaek/zakaz.php
+++ urnaek/zakaz.php
@@ -4,7 +4,7 @@
     $zakaz = array("ru" => "СДЕЛАТЬ ЗАКАЗ", "tt" => "ЗАКАЗ ЯСЫЙМ");
 ?>
 <form method="post" align="center">
-    <p><input name="isem" placeholder="<?=$isem[$APPLICATION->tele]?>" type="text" required="" /></p>
-    <p><input name="tilifun" id="tilifun" placeholder="<?=$tilifun[$APPLICATION->tele]?>" type="tel" required="" /></p>
-    <p><input name="zakaz_jasaw" value="<?=$zakaz[$APPLICATION->tele]?>" type="submit" /></p>
+    <p><input class="input-text" name="isem" id="isem" placeholder="<?=$isem[$APPLICATION->tele]?>" type="text" required="" /></p>
+    <p><input class="input-text" name="tilifun" id="tilifun" placeholder="<?=$tilifun[$APPLICATION->tele]?>" type="tel" required="" /></p>
+    <p><input class="input-button" name="zakaz_jasaw" value="<?=$zakaz[$APPLICATION->tele]?>" type="submit" /></p>
 </form>
