4bdcbbf8f1e1ddda6fa1419edf970e54b3fc4cb8
Mansur Usmanov <gho-mansur@yandex.ru>
2016-12-26 01:11:21 +0300

    phone

--- bash.php
+++ bash.php
@@ -15,7 +15,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         
         <script type="text/javascript" src="/script/jquery.maskedinput.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="/bizək/style.css?t=4"/>
+	<link rel="stylesheet" type="text/css" href="/bizək/style.css?t=5"/>
 	<link rel="alternate" hreflang="ru" href="http://ostalar.tk/ru" />
 	<link rel="alternate" hreflang="tt" href="http://ostalar.tk/tt" />
 	<meta charset="utf-8"/>
