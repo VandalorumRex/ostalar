158ebdf45843c210b6e80b6087a3e402680fb25a
Mansur Usmanov <gho-mansur@yandex.ru>
2016-12-25 23:24:24 +0300

    menu

--- menu.php
+++ menu.php
@@ -2,9 +2,20 @@
 $menu = simplexml_load_string(file_get_contents("menu.xml"));
 //print '<pre>';print_r($menu);print '</pre>';
 ?>
-<div class="muyyn">
-    <div class="col-md-9">
-        <ul id="menu_uze">
+<nav role="navigation" class="navbar navbar-default muyyn">
+    <div class="navbar-header">
+	<button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+	    <span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	</button>
+	<!--a href="#" class="navbar-brand">БАШ</a-->
+    </div>
+    
+    <div id="navbarCollapse" class="collapse navbar-collapse">
+        
+        <ul class="nav navbar-nav">
                 <?php foreach($menu as $item):
                     $it = (array)$item;
                     $name = $APPLICATION->tele=='ru'?$it['@attributes']['name']:$it['@attributes']['tt'];
@@ -25,23 +36,29 @@ $menu = simplexml_load_string(file_get_contents("menu.xml"));
                         </ul>
                     <?endif*/?>
                     </li>
-                <?php endforeach?>			
-		</ul>
-    </div>
-    <div class="col-md-3 find_line">
-        <!--div><gcse:search></gcse:search></div-->
-        <table>
-            <tr>
-                <td><input type="search" class="ezläw" id="ezläw" list="törlər" /></td>
+                <?php endforeach?>
+        </ul>
+                     <form role="search" class="navbar-form navbar-right">
+                        <!--div><gcse:search></gcse:search></div-->
+                        <div class="form-group col-md-12">
+                            <table>
+                                <tr>
+                                    <td><input type="search" class="ezläw" id="ezläw" list="törlər" class="form-control"/></td>
+
+                                    <td><div id="ezlägech"></div></td>
+                                </tr>
+                            </table>
+                            <datalist id="törlər">
+                                <?php foreach (Törlər::$ISEMNƏRE[$APPLICATION->tele] as $isem):?>
+                                <option value="<?=$isem?>"></option>
+                                <?php endforeach;?>
+                            </datalist>
+                        </div>
+                    </form>
+		
         
-                <td><div id="ezlägech"></div></td>
-            </tr>
-        </table>
-        <datalist id="törlər">
-            <?php foreach (Törlər::$ISEMNƏRE[$APPLICATION->tele] as $isem):?>
-            <option value="<?=$isem?>"></option>
-            <?php endforeach;?>
-        </datalist>
+       
+        
     </div>
     <script>
         $("#ezlägech").click(function(){
@@ -49,4 +66,4 @@ $menu = simplexml_load_string(file_get_contents("menu.xml"));
         });
     </script>
         
-</div>
+</nav>
